c7c52543f2c81ee1edf6f6effcc8ff1d82162163
Levi Januario <pereiralevi115@gmail.com>
2026-01-14 15:33:07 +0000

    File changes

--- src/pages/Graficos.jsx
+++ src/pages/Graficos.jsx
@@ -111,7 +111,7 @@ export default function Graficos() {
   });
   const chartData = Object.values(monthlyData)
     .filter(d => d.liters > 0 || d.km > 0 || d.cost > 0)
-    .sort((a, b) => monthNames.indexOf(a.name) - monthNames.indexOf(b.name));
+    .sort((a, b) => a.cost - b.cost);
 
   // By unit
   const byUnitData = units.map(unit => {
