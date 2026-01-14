980eb97e27dd0459d76bf6cfb32f950aa13414fa
Levi Januario <pereiralevi115@gmail.com>
2026-01-14 15:29:01 +0000

    File changes

--- src/pages/Graficos.jsx
+++ src/pages/Graficos.jsx
@@ -325,8 +325,8 @@ export default function Graficos() {
         <ResponsiveContainer width="100%" height={450}>
           <BarChart data={byUnitData} margin={{ top: 30, right: 30, left: 70, bottom: 20 }}>
             <CartesianGrid strokeDasharray="2 4" stroke="#475569" vertical={false} />
-            <XAxis dataKey="name" angle={-45} textAnchor="end" height={150} stroke="#94a3b8" />
-            <YAxis stroke="#94a3b8" />
+            <XAxis dataKey="name" angle={-45} textAnchor="end" height={150} stroke="#94a3b8" label={false} />
+            <YAxis stroke="#94a3b8" label={false} />
             <Tooltip content={<CustomTooltip />} />
             <Bar dataKey="liters" fill={YELLOW} name="Litros" radius={[4, 4, 0, 0]} />
             <Bar dataKey="km" fill={BLUE} name="Km" radius={[4, 4, 0, 0]} />
