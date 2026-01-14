299dce4b3ae63a3c583e38db8b7949cfb7e311b9
Levi Januario <pereiralevi115@gmail.com>
2026-01-14 18:42:24 +0000

    File changes

--- src/pages/Graficos.jsx
+++ src/pages/Graficos.jsx
@@ -598,7 +598,7 @@ export default function Graficos() {
               <YAxis type="category" hide={true} />
               <Tooltip content={<CustomTooltip />} />
               <Bar dataKey="kmPerLiter" fill={YELLOW} radius={[0, 4, 4, 0]} label={<VehicleKmLiterLabel />}>
-                 <LabelList dataKey="placa" position="insideLeft" fill="#ffffff" fontSize={10} fontWeight="600" />
+                 <LabelList dataKey="placa" position="insideLeft" fill="#000000" fontSize={10} fontWeight="600" />
                 </Bar>
                 </BarChart>
                 </ResponsiveContainer>
