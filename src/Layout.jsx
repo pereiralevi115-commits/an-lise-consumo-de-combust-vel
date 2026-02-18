9b0e016d5119dd5d606c2ebd0d0ca25865e922cc
Levi Januario <pereiralevi115@gmail.com>
2026-02-18 20:58:46 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -8,7 +8,7 @@ export default function Layout({ children, currentPageName }) {
     { name: 'Gráficos', page: 'Graficos', icon: BarChart3 },
     { name: 'Dados', page: 'Dados', icon: Table2 },
     { name: 'Upload', page: 'Upload', icon: Upload },
-    { name: 'Legendas', page: 'Legendas', icon: Upload }
+    { name: 'Legendas', page: 'Legendas', icon: BookOpen }
   ];
 
   return (
