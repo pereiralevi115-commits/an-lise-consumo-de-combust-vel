caf2301acfda1c9ab07f7598ee3831f66058ba08
Levi Januario <pereiralevi115@gmail.com>
2026-02-20 11:13:08 +0000

    File changes

--- src/pages/Dados.jsx
+++ src/pages/Dados.jsx
@@ -70,7 +70,7 @@ export default function Dados() {
 
   // Detectar inconsistências de KM por placa
   const kmInconsistencyIds = new Set();
-  const KM_MAX_DIFF = 2000;
+  const KM_MAX_DIFF = 1100;
 
   const plateGroups = {};
   records.forEach(r => {
