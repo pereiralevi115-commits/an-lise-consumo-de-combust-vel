029d7b8836bdce3a16b4506c6ed56a063bddd9f4
Levi Januario <pereiralevi115@gmail.com>
2026-03-02 15:44:50 +0000

    File changes

--- src/pages/AnalisePorPlaca.jsx
+++ src/pages/AnalisePorPlaca.jsx
@@ -441,6 +441,7 @@ export default function AnalisePorPlaca() {
                     );
                   })
                 )}
+
               </TableBody>
             </Table>
           </div>
