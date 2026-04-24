4f099e3462b4a7e7ce610179e8cf5b1ab00fe7ae
Levi Januario <pereiralevi115@gmail.com>
2026-04-24 20:37:34 +0000

    File changes

--- src/pages/Legendas.jsx
+++ src/pages/Legendas.jsx
@@ -323,6 +323,7 @@ export default function Legendas() {
       </div>
 
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mb-6">
+        <ValorCalculado />
         <TotalM3Card />
       </div>
 
