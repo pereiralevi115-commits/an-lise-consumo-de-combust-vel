4863a6588e30d652a9912503cfa32a05e019157b
Levi Januario <pereiralevi115@gmail.com>
2026-04-30 11:56:54 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -68,7 +68,7 @@ export default function Layout({ children, currentPageName }) {
       </header>
 
       {/* CONTEÚDO */}
-      <main className="max-w-[1600px] mx-auto px-4 md:px-8 py-8">
+      <main className="max-w-[2000px] mx-auto px-4 md:px-8 py-8">
         {children}
       </main>
 
